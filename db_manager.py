b78aa19c8d40cf0f1a78d5de283c047fddde9734
marionl4b <marion.labarre@gmail.com>
2018-11-19 21:15:29 +0100

    Add insert_data(), prepare data to insert in database

--- db_manager.py
+++ db_manager.py
@@ -15,6 +15,7 @@ class DataInit:
 
         self.use_database()
         self.create_tables()
+        self.insert_data()
 
     def database_connexion(self):
         """test config and connect to mysql server"""
@@ -68,3 +69,63 @@ class DataInit:
                 else:
                     print(err.msg)
                     exit(1)
+
+    def insert_data(self):
+        """select table defined in settings and specify insertion query"""
+        for table in settings.TABLES:
+            if table == "category":
+                query = "INSERT IGNORE INTO category (category_name) VALUES (%(name)s)"
+                self.data_dict_constructor("category", query, self.categories)
+            elif table == "product":
+                query = "INSERT IGNORE INTO product (product_name, description, brand, API_link, nutriscore)" \
+                        "VALUES (%(name)s, %(desc)s, %(brand)s, %(url)s, %(nutriscore)s)"
+                self.data_dict_constructor("product", query, self.products)
+            elif table == "store":
+                query = "INSERT IGNORE INTO store (store_name) VALUES (%(name)s)"
+                self.data_dict_constructor("store", query, self.products)
+            elif table == "category_product":
+                query = "INSERT IGNORE INTO category_product (cp_category, cp_product) " \
+                        "VALUES (%(category)s, %(product)s)"
+                self.data_dict_constructor("category_product", query, self.products)
+            elif table == "store_product":
+                query = "INSERT IGNORE INTO store_product (sp_store, sp_product) " \
+                        "VALUES (%(store)s, %(product)s)"
+                self.data_dict_constructor("store_product", query, self.products)
+            else:
+                pass
+        print("data inserted")
+
+    def data_dict_constructor(self, table, query, crawling_list):
+        """select data for each table and store it in clean dictionaries"""
+        for item in crawling_list:
+            if table == "category" or table == "product":
+                data_dict = item
+                self.check_data(table, query, data_dict)
+            elif table == "store":
+                stores = item["store"].split(",")
+                for store in stores:
+                    data_dict = {"name": store}
+                    self.check_data(table, query, data_dict)
+            elif table == "category_product":
+                prod_categories = item["categories"].split(",")
+                for cat in prod_categories:
+                    data_dict = {"category": cat, "product": item["name"]}
+                    self.check_data(table, query, data_dict)
+            elif table == "store_product":
+                prod_stores = item["store"].split(",")
+                for store in prod_stores:
+                    data_dict = {"store": store, "product": item["name"]}
+                    self.check_data(table, query, data_dict)
+            else:
+                pass
+
+    def check_data(self, table, query, data_dict):
+        """insert data from temp dictionnary into table if don't already exists"""
+        try:
+            self.cursor.execute("SELECT EXISTS(SELECT * FROM {})".format(table))
+        except mysql.connector.Error as err:
+            print("Error: {}".format(err))
+            self.cnx.rollback()
+        else:
+            self.cursor.execute(query, data_dict)
+            self.cnx.commit()
